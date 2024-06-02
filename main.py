31efb8ddadf057dfc66261a08a921a82130bdf1c
Atanu Das <malatiexe@gmail.com>
2024-06-02 06:42:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,30 @@
 import turtle
 import pandas
 
+states = pandas.read_csv("E:\\python\\udemy_python\\us_state_guess\\50_states.csv")
+states['state'] = states['state'].str.lower()
+
 screen = turtle.Screen()
-screen.title("U.S. States Game")
-image = "./us-states-game-start/blank_states_img.gif"
+
+
+image = "E:\\python\\udemy_python\\us_state_guess\\blank_states_img.gif"
 screen.addshape(image)
-turtle.shape(image)
+map_turtle = turtle.Turtle()  # Create a new turtle for the map
+map_turtle.shape(image)
+screen.title("US. State guess")
 
-data = pandas.read_csv("./us-states-game-start/50_states.csv")
-all_states = data.state.to_list()
-guessed_states = []
+state_guessed = 0
 
-while len(guessed_states) < 50:
-    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
-                                    prompt="What's another state's name?").title()
-    if answer_state == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
-        new_data = pandas.DataFrame(missing_states)
-        new_data.to_csv("states_to_learn.csv")
+while True:
+    guess_state = screen.textinput(title=f"{state_guessed}/50 state guessed", prompt="What's another state name?")
+    if guess_state is None:
         break
-    if answer_state in all_states:
-        guessed_states.append(answer_state)
-        t = turtle.Turtle()
-        t.hideturtle()
-        t.penup()
-        state_data = data[data.state == answer_state]
-        t.goto(int(state_data.x), int(state_data.y))
-        t.write(answer_state)
+    guess_state = guess_state.lower()
+    if guess_state in states.state.to_list():
+        state_data = states[states.state == guess_state]
+        writer_turtle = turtle.Turtle()  # Create a new turtle for writing
+        writer_turtle.hideturtle()  # Hide the turtle
+        writer_turtle.penup()
+        writer_turtle.goto(int(state_data.x.iloc[0]), int(state_data.y.iloc[0]))
+        writer_turtle.write(guess_state)
+        state_guessed += 1
